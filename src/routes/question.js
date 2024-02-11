a24ed2c6d573639ff9bdf8a091ceb81691249516
dassy <professionaldassy@gmail.com>
2024-02-11 19:33:07 -0300

    feat: send && receive form data

--- src/routes/question.js
+++ src/routes/question.js
@@ -5,3 +5,12 @@ export const question = Router()
 question.get('/question', (req, res) => {
   res.render('question')
 })
+
+question.post('/question', (req, res) => {
+  const title = req.body.title
+  const description = req.body.description
+
+  console.log(`${title} ${description}`)
+
+  res.send('formulario enviado!')
+})
--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
 import { question } from './routes/question.js'
 
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 // estou dizendo para o express que a view engine utilizada sera o EJS.
 app.set('view engine', 'ejs')
